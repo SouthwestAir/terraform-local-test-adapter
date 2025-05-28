acb7aa8ddf61b4f055f466ac6ae3c1af4a28610d
Arseny Andriushchenko <Arseny.Andriushchenko@wnco.com>
2025-05-28 10:17:27 -0400

    fix: SWA-00 lower required provider versions

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">1.10.0"
+  required_version = ">1.9.0"
   required_providers {
     # tflint-ignore: terraform_unused_required_providers
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.3"
+      version = ">= 3.0.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.4.0"
+      version = ">= 2.0.0"
     }
   }
 }
